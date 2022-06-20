1d536e35b3b9c68fa8bc30d02e98b38fb5fa8842
Denis Dashkevich <majorprestige99@gmail.com>
2022-06-20 13:33:23 +0300

    1

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// console.log("hello");
+// console.log(5);
+
+// let username = "oleg";
+// console.log(username);
+
+// username = "Max";
+// console.log(username);
+
+// const username = prompt("Ваше имя");
+// console.log(username);
+
+// const isAdult = confirm("Вам есть 18?");
+// console.log(isAdult);
+
+// const numA = 10;
+// const numB = 20;
+
+// const result = numA % 2;
+
+// console.log(result === 0);
+
+// const totalMinutes = 90;
+// let hours = totalMinutes / 60;
+// hours = Math.floor(hours);
+// const minutes = totalMinutes % 60;
+// console.log(hours ":" minutes);
+
+// ! snippet na console.log
+// let userNumber = prompt("введите число");
+
+// const strA = "     hello";
+// console.log(strA);
