e7e2e83c67d0244a1ad74dbecdc58b8a7bf8eb19
Denis Dashkevich <majorprestige99@gmail.com>
2022-06-20 18:33:39 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,3 +31,67 @@
 
 // const strA = "     hello";
 // console.log(strA);
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+// Change code below this line
+// 	const message = `You ordered droids worth ${
+// 		orderedQuantity + pricePerDroid + deliveryFee
+// 	} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// 	console.log(message);
+// Change code above this line
+// 	return message;
+// }
+
+// makeOrderMessage(10, 70, 200);
+
+// function checkIfCanAccessContent(subType) {
+// 	const canAccessContent = subType === pro || subType === vip; // Change this line
+
+// 	return canAccessContent;
+// }
+
+// checkIfCanAccessContent("pro");
+
+// const courseTopic = "JavaScript essentials";
+// // Change code below this line
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[20];
+
+// console.log(courseTopic[20]);
+
+// function getSubstring(string, length) {
+//   const substring = ; // Change this line
+
+//   return substring;
+// }
+
+// getSubstring()
+
+// function formatMessage(message, maxLength) {
+// 	let result;
+// 	// Change code below this line
+// 	if (message <= maxLength) {
+// 		result = message;
+// 	} else {
+// 		result = message.slice(0, maxLength) + "...";
+// 	}
+// 	console.log(result);
+// 	/// Change code above this line
+// 	return result;
+// }
+
+// formatMessage("Nunc sed turpis a felis in nunc fringilla", 300);
+
+function checkForSpam(message) {
+	let result;
+	// Change code below this line
+	result = message.includes("spam" || "sale") ? true : false;
+
+	console.log(result);
+	// Change code above this line
+	return result;
+}
+
+checkForSpam("[SPAM] How to earn fast money?");
