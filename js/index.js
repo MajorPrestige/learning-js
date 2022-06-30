136833a55b8125b95216da628a3f08fad7c0dda3
Denis Dashkevich <majorprestige99@gmail.com>
2022-06-30 12:06:58 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -534,3 +534,192 @@
 // atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
 // // console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"));
 // atTheOldToad.getPotions();
+
+// function makePizza(pizzaName, callback) {
+// 	console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+// 	callback(pizzaName);
+// }
+
+// // makePizza("Royal Grand", function deliverPizza(pizzaName) {
+// // 	console.log(`Delivering pizza ${pizzaName}.`);
+// // });
+// // Change code below this line
+
+// makePizza("Ultracheese", (eatPizza = pizzaName => console.log(`Eating pizza ${pizzaName}`)));
+
+// // !==
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+// 	console.log(`Eating pizza ${pizzaName}`);
+// });
+
+// const pizzaPalace = {
+// 	pizzas: ["Ultracheese", "Smoked", "Four meats"],
+// 	order(pizzaName, onSuccess, onError) {
+// 		for (const pizza of this.pizzas) {
+// 			if (pizzaName === pizza) {
+// 				return console.log(onSuccess(pizzaName));
+// 			}
+// 		}
+// 		console.log(onError(`There is no pizza with a name ${pizzaName} in the assortment.`));
+// 	},
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+// 	return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+// 	return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+
+// calculateTotalPrice = (quantity, pricePerItem) => {
+// 	return quantity * pricePerItem;
+// };
+// console.log(calculateTotalPrice(5, 100));
+
+// function changeEven(numbers, value) {
+// 	// Change code below this line
+
+// 	const newArr = [];
+// 	for (let i = 0; i < numbers.length; i += 1) {
+// 		if (numbers[i] % 2 === 0) {
+// 			newArr.push(numbers[i] + value);
+// 			continue;
+// 		}
+// 		newArr.push(numbers[i]);
+// 	}
+
+// 	return console.log(newArr);
+// 	// Change code above this line
+// }
+
+// changeEven([1, 2, 3, 4, 5], 10);
+
+// const books = [
+// 	{
+// 		title: "The Last Kingdom",
+// 		author: "Bernard Cornwell",
+// 		rating: 8.38,
+// 	},
+// 	{
+// 		title: "Beside Still Waters",
+// 		author: "Robert Sheckley",
+// 		rating: 8.51,
+// 	},
+// 	{
+// 		title: "The Dream of a Ridiculous Man",
+// 		author: "Fyodor Dostoevsky",
+// 		rating: 7.75,
+// 	},
+// 	{ title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+// 	{ title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// // Change code below this line
+
+// const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING);
+// const booksByAuthor = books.filter(({ author }) => (author = AUTHOR));
+
+// console.log(booksByAuthor);
+// console.log(topRatedBooks);
+
+const abc = [
+	{
+		name: "Moore Hensley",
+		email: "moorehensley@indexia.com",
+		eyeColor: "blue",
+		friends: ["Sharron Pace"],
+		isActive: false,
+		balance: 2811,
+		skills: ["ipsum", "lorem"],
+		gender: "male",
+		age: 37,
+	},
+	{
+		name: "Sharlene Bush",
+		email: "sharlenebush@tubesys.com",
+		eyeColor: "blue",
+		friends: ["Briana Decker", "Sharron Pace"],
+		isActive: true,
+		balance: 3821,
+		skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+		gender: "female",
+		age: 34,
+	},
+	{
+		name: "Ross Vazquez",
+		email: "rossvazquez@xinware.com",
+		eyeColor: "green",
+		friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+		isActive: false,
+		balance: 3793,
+		skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+		gender: "male",
+		age: 24,
+	},
+	{
+		name: "Elma Head",
+		email: "elmahead@omatom.com",
+		eyeColor: "green",
+		friends: ["Goldie Gentry", "Aisha Tran"],
+		isActive: true,
+		balance: 2278,
+		skills: ["adipisicing", "irure", "velit"],
+		gender: "female",
+		age: 21,
+	},
+	{
+		name: "Carey Barr",
+		email: "careybarr@nurali.com",
+		eyeColor: "blue",
+		friends: ["Jordan Sampson", "Eddie Strong"],
+		isActive: true,
+		balance: 3951,
+		skills: ["ex", "culpa", "nostrud"],
+		gender: "male",
+		age: 27,
+	},
+	{
+		name: "Blackburn Dotson",
+		email: "blackburndotson@furnigeer.com",
+		eyeColor: "brown",
+		friends: ["Jacklyn Lucas", "Linda Chapman"],
+		isActive: false,
+		balance: 1498,
+		skills: ["non", "amet", "ipsum"],
+		gender: "male",
+		age: 38,
+	},
+	{
+		name: "Sheree Anthony",
+		email: "shereeanthony@kog.com",
+		eyeColor: "brown",
+		friends: ["Goldie Gentry", "Briana Decker"],
+		isActive: true,
+		balance: 2764,
+		skills: ["lorem", "veniam", "culpa"],
+		gender: "female",
+		age: 39,
+	},
+];
+
+const getSortedFriends = users => {
+	return users
+		.flatMap(({ friends }) => friends)
+		.filter((el, idx, arr) => arr.indexOf(el) === idx)
+		.sort((a, b) => a.localeCompare(b));
+};
+
+console.log(getSortedFriends(abc));
