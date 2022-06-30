dfb8622e72faaedb16e5d899ed8c155f93b57a38
Denis Dashkevich <majorprestige99@gmail.com>
2022-06-30 13:06:55 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -635,91 +635,116 @@
 // console.log(booksByAuthor);
 // console.log(topRatedBooks);
 
-const abc = [
-	{
-		name: "Moore Hensley",
-		email: "moorehensley@indexia.com",
-		eyeColor: "blue",
-		friends: ["Sharron Pace"],
-		isActive: false,
-		balance: 2811,
-		skills: ["ipsum", "lorem"],
-		gender: "male",
-		age: 37,
-	},
-	{
-		name: "Sharlene Bush",
-		email: "sharlenebush@tubesys.com",
-		eyeColor: "blue",
-		friends: ["Briana Decker", "Sharron Pace"],
-		isActive: true,
-		balance: 3821,
-		skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-		gender: "female",
-		age: 34,
-	},
-	{
-		name: "Ross Vazquez",
-		email: "rossvazquez@xinware.com",
-		eyeColor: "green",
-		friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-		isActive: false,
-		balance: 3793,
-		skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-		gender: "male",
-		age: 24,
-	},
-	{
-		name: "Elma Head",
-		email: "elmahead@omatom.com",
-		eyeColor: "green",
-		friends: ["Goldie Gentry", "Aisha Tran"],
-		isActive: true,
-		balance: 2278,
-		skills: ["adipisicing", "irure", "velit"],
-		gender: "female",
-		age: 21,
-	},
-	{
-		name: "Carey Barr",
-		email: "careybarr@nurali.com",
-		eyeColor: "blue",
-		friends: ["Jordan Sampson", "Eddie Strong"],
-		isActive: true,
-		balance: 3951,
-		skills: ["ex", "culpa", "nostrud"],
-		gender: "male",
-		age: 27,
-	},
-	{
-		name: "Blackburn Dotson",
-		email: "blackburndotson@furnigeer.com",
-		eyeColor: "brown",
-		friends: ["Jacklyn Lucas", "Linda Chapman"],
-		isActive: false,
-		balance: 1498,
-		skills: ["non", "amet", "ipsum"],
-		gender: "male",
-		age: 38,
-	},
-	{
-		name: "Sheree Anthony",
-		email: "shereeanthony@kog.com",
-		eyeColor: "brown",
-		friends: ["Goldie Gentry", "Briana Decker"],
-		isActive: true,
-		balance: 2764,
-		skills: ["lorem", "veniam", "culpa"],
-		gender: "female",
-		age: 39,
-	},
-];
+// const abc = [
+// 	{
+// 		name: "Moore Hensley",
+// 		email: "moorehensley@indexia.com",
+// 		eyeColor: "blue",
+// 		friends: ["Sharron Pace"],
+// 		isActive: false,
+// 		balance: 2811,
+// 		skills: ["ipsum", "lorem"],
+// 		gender: "male",
+// 		age: 37,
+// 	},
+// 	{
+// 		name: "Sharlene Bush",
+// 		email: "sharlenebush@tubesys.com",
+// 		eyeColor: "blue",
+// 		friends: ["Briana Decker", "Sharron Pace"],
+// 		isActive: true,
+// 		balance: 3821,
+// 		skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+// 		gender: "female",
+// 		age: 34,
+// 	},
+// 	{
+// 		name: "Ross Vazquez",
+// 		email: "rossvazquez@xinware.com",
+// 		eyeColor: "green",
+// 		friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+// 		isActive: false,
+// 		balance: 3793,
+// 		skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+// 		gender: "male",
+// 		age: 24,
+// 	},
+// 	{
+// 		name: "Elma Head",
+// 		email: "elmahead@omatom.com",
+// 		eyeColor: "green",
+// 		friends: ["Goldie Gentry", "Aisha Tran"],
+// 		isActive: true,
+// 		balance: 2278,
+// 		skills: ["adipisicing", "irure", "velit"],
+// 		gender: "female",
+// 		age: 21,
+// 	},
+// 	{
+// 		name: "Carey Barr",
+// 		email: "careybarr@nurali.com",
+// 		eyeColor: "blue",
+// 		friends: ["Jordan Sampson", "Eddie Strong"],
+// 		isActive: true,
+// 		balance: 3951,
+// 		skills: ["ex", "culpa", "nostrud"],
+// 		gender: "male",
+// 		age: 27,
+// 	},
+// 	{
+// 		name: "Blackburn Dotson",
+// 		email: "blackburndotson@furnigeer.com",
+// 		eyeColor: "brown",
+// 		friends: ["Jacklyn Lucas", "Linda Chapman"],
+// 		isActive: false,
+// 		balance: 1498,
+// 		skills: ["non", "amet", "ipsum"],
+// 		gender: "male",
+// 		age: 38,
+// 	},
+// 	{
+// 		name: "Sheree Anthony",
+// 		email: "shereeanthony@kog.com",
+// 		eyeColor: "brown",
+// 		friends: ["Goldie Gentry", "Briana Decker"],
+// 		isActive: true,
+// 		balance: 2764,
+// 		skills: ["lorem", "veniam", "culpa"],
+// 		gender: "female",
+// 		age: 39,
+// 	},
+// ];
 
-const getSortedFriends = users => {
-	return users
-		.flatMap(({ friends }) => friends)
-		.filter((el, idx, arr) => arr.indexOf(el) === idx)
-		.sort((a, b) => a.localeCompare(b));
-};
+// const getSortedFriends = users => {
+// 	return users
+// 		.flatMap(({ friends }) => friends)
+// 		.filter((el, idx, arr) => arr.indexOf(el) === idx)
+// 		.sort((a, b) => a.localeCompare(b));
+// };
 
-console.log(getSortedFriends(abc));
+// console.log(getSortedFriends(abc));
+
+// const chopShop = {
+// 	stones: [
+// 		{ name: "Emerald", price: 1300, quantity: 4 },
+// 		{ name: "Diamond", price: 2700, quantity: 3 },
+// 		{ name: "Sapphire", price: 1400, quantity: 7 },
+// 		{ name: "Ruby", price: 800, quantity: 2 },
+// 	],
+
+// 	calcTotalPrice(stoneName) {
+// 		const total = this.stones.reduce((issue, el) => {
+// 			if (el.name === stoneName) {
+// 				return el.price * el.quantity;
+// 			}
+// 			return issue;
+// 		}, "Такого каменя не знайдено");
+// 		return total;
+// 	},
+// };
+
+// console.log(chopShop.calcTotalPrice("Emerald")); // 5200
+// console.log(chopShop.calcTotalPrice("Diamond")); // 8100
+// console.log(chopShop.calcTotalPrice("Sapphire")); // 9800
+// console.log(chopShop.calcTotalPrice("Ruby")); // 1600
+// console.log(chopShop.calcTotalPrice("Sdfd"));
